b36dbff008218773e0b9d352d94893724a5eabce
Devagn Maniya <99108074+devagn611@users.noreply.github.com>
2022-07-22 19:39:02 +0530

    Third_set

---
+++ Practical_3/Prac_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("My name is Maniya Devagn. \nMy Enrolment Number is 210210116004.\n");
+    int ans = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        ans+=i;
+        
+    }
+    printf("%d", ans);
+    return 0;
+}
